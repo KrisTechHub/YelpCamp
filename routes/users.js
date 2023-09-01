5be712ce63f56a91745af6fd7965ce39753cb8a9
kristechhub <kmj.algario@gmail.com>
2023-09-01 12:17:48 +0200

    setting up log in

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utilities/catchAsync');
 const User = require('../models/user');
 
+
+//REGISTER
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-    const { email, username, password } = req.body //get info from the req.body
-    const user = new User({ email, username }); //pass info in the form to a new user in database
-    const registeredUser = await User.register(user, password);
-    console.log(registeredUser);
-    req.flash('Welcome to YelpCamp!');
-    res.redirect('/campgrounds');
-})
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body //get info from the req.body
+        const user = new User({ email, username }); //pass info in the form to a new user in database
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'You have successfully registered. Welcome to YelpCamp!');
+        res.redirect('/campgrounds');
+    } catch (e) {
+        req.flash('error', e.message)
+        res.redirect('/register')
+    }
+}));
+
+
+//LOGIN
+router.get('/login', catchAsync(async (req, res) => {
+
+}))
 
 module.exports = router;
