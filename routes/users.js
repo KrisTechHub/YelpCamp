38b4940a10a16d1f214eca08ef423c3420f2c726
kristechhub <kmj.algario@gmail.com>
2023-09-01 11:25:29 +0200

    successfully saved user to database

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,12 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    res.send(req.body)
+    const { email, username, password } = req.body //get info from the req.body
+    const user = new User({ email, username }); //pass info in the form to a new user in database
+    const registeredUser = await User.register(user, password);
+    console.log(registeredUser);
+    req.flash('Welcome to YelpCamp!');
+    res.redirect('/campgrounds');
 })
 
 module.exports = router;
