88e2498c53f4698cf8ea4e5cc37f003e6cbc09d2
kristechhub <kmj.algario@gmail.com>
2023-09-05 14:25:41 +0200

    adding lcoudinary

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== "production") { //enviroment variable thats usually
     require('dotenv').config();
 }
 
-console.log(process.env.SECRET);
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
 
 const express = require('express');
 const path = require('path');
